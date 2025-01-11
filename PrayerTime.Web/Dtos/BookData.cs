22cddee8bd966bf1bcf99309f2dc61d261cf49c2
Rakhimboyev-Feruzbek <feruzbekraximboyev2700@gmail.com>
2025-01-11 23:22:03 +0500

    Second Page Added

---
+++ PrayerTime.Web/Dtos/BookData.cs
@@ -0,0 +1,10 @@
+namespace PrayerTime.Web.Dtos;
+
+public class BookData
+{
+    public int Id { get; set; }
+    public string? BookName { get; set; }
+    public string? Author { get; set; }
+    public string? FocusArea { get; set; }
+    public string? AmazonLink { get; set; }
+}
